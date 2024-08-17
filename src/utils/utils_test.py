2436a8623f948ccb41f00551995c4909e31df3ec
Stelios Daveas <sdaveas@gmail.com>
2024-08-17 23:30:50 +0300

    chore: add some tests and ignore pytest artifacts

--- src/utils/utils_test.py
+++ src/utils/utils_test.py
@@ -7,7 +7,11 @@ def test_single_unit_pytest():
     assert time_expression_to_seconds('1d') == 24 * 60 * 60
     assert time_expression_to_seconds('1d1m') == 24 * 60 * 60 + 60
     assert time_expression_to_seconds('1d1h1m') == 24 * 60 * 60 + 60 * 60 + 60
+
     # test reverse order
     assert time_expression_to_seconds('1m1h1d') == 24 * 60 * 60 + 60 * 60 + 60
 
+    # test invalid cases
     assert time_expression_to_seconds('stelios') == 0
+    assert time_expression_to_seconds('1') == 0
+    assert time_expression_to_seconds('m') == 0
