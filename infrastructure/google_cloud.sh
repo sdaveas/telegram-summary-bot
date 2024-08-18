e8ae03b07938be728a14b9c29e01e3fc8560f7ee
Stelios Daveas <sdaveas@gmail.com>
2024-08-18 16:54:35 +0300

    cicd: add bucket to store database

--- infrastructure/google_cloud.sh
+++ infrastructure/google_cloud.sh
@@ -29,18 +29,16 @@ docker push gcr.io/$PROJECT_ID/$IMAGE_NAME:$IMAGE_TAG
 
 # Deploy to Cloud Run
 gcloud run deploy \
+--service-name telegram-summary-bot \
 --image gcr.io/$PROJECT_ID/$IMAGE_NAME:$IMAGE_TAG \
 --port 8080 \
---set-env-vars TELEGRAM_BOT_TOKEN="$TELEGRAM_BOT_TOKEN",ANTHROPIC_API_KEY="$ANTHROPIC_API_KEY",DATABASE_PATH="$DATABASE_PATH"
+--set-env-vars TELEGRAM_BOT_TOKEN="$TELEGRAM_BOT_TOKEN",ANTHROPIC_API_KEY="$ANTHROPIC_API_KEY",DATABASE_PATH="$DATABASE_PATH" \
+--region europe-central2
 
-if [ -z "$DEPLOY_BUCKET" ]; then
-    echo "Not creating a new bucket for the deployment"
-    exit 1
-else
-    echo "Creating a new bucket for the deployment"
-	gcloud beta run services update telegram-summary-bot \
-        --add-volume name=telegram-summary-bot-database,type=cloud-storage,bucket=telegram-summary-bot-database \
-        --add-volume-mount volume=telegram-summary-bot-database,mount-path=/app/database
-fi 
+echo "Creating a new bucket for the deployment"
+gcloud beta run services update telegram-summary-bot \
+    --region europe-central2 \
+    --add-volume name=telegram-summary-bot-database,type=cloud-storage,bucket=telegram-summary-bot-database \
+    --add-volume-mount volume=telegram-summary-bot-database,mount-path=/app/database
 
 echo "Deployment complete. Check the Google Cloud Console for your app's URL."
